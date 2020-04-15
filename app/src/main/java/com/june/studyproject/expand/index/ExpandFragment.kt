6210383ddcc43866e592ca977cc0e55af029f176
June <junemvp@foxmail.com>
2020-04-15 18:18:02 +0800

    CustomLayoutManager优化

--- app/src/main/java/com/june/studyproject/expand/index/ExpandFragment.kt
+++ app/src/main/java/com/june/studyproject/expand/index/ExpandFragment.kt
@@ -2,6 +2,7 @@ package com.june.studyproject.expand.index
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.june.studyproject.R
 import com.june.studyproject.base.component.BaseFragment
 import com.june.studyproject.common.ConstHelper
@@ -9,6 +10,7 @@ import com.june.studyproject.component.index.CardExampleVo
 import com.june.studyproject.component.recycler.custom.CustomLayoutManager
 import com.june.studyproject.expand.image.box.ImageBoxActivity
 import kotlinx.android.synthetic.main.fragment_expand.*
+import timber.log.Timber
 
 class ExpandFragment : BaseFragment() {
 
@@ -25,6 +27,28 @@ class ExpandFragment : BaseFragment() {
         rvExpand.layoutManager = CustomLayoutManager(requireActivity())
         rvExpand.adapter = adapter
         rvExpand.setHasFixedSize(true)
+
+        rvExpand.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                val state = when (newState) {
+                    //RecyclerView当前未滚动
+                    RecyclerView.SCROLL_STATE_IDLE -> "停止"
+                    //RecyclerView当前正在由外部输入（例如用户触摸输入）拖动
+                    RecyclerView.SCROLL_STATE_DRAGGING -> "拖动"
+                    //RecyclerView当前处于动画最终状态，而不受外部控制
+                    RecyclerView.SCROLL_STATE_SETTLING -> "不受外部控制的最终状态"
+                    else -> "unKnow"
+                }
+                Timber.e("onScrollStateChanged : $state")
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val customLayoutManager = recyclerView.layoutManager as CustomLayoutManager
+                var findFirstVisibleItemPosition = customLayoutManager.findFirstVisibleItemPosition()
+                var findLastVisibleItemPosition = customLayoutManager.findLastVisibleItemPosition()
+                Timber.e("onScrolled==>first:$findFirstVisibleItemPosition \nlast:$findLastVisibleItemPosition")
+            }
+        })
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
