850e6995bce35715eb3d978a036749c994ba6f7a
June <junemvp@foxmail.com>
2021-08-03 15:36:13 +0800

    测试微信头像展示

---
+++ app/src/main/java/com/june/studyproject/ssss.java
@@ -0,0 +1,20 @@
+package com.june.studyproject;
+
+public class ssss {
+
+
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = new String("abc");
+
+        if (s1 == s2) {
+
+        }
+    }
+
+    class User{
+        String name;
+        int age;
+    }
+}
+
