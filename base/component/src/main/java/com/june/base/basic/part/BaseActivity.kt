bb9320844613de7bb8b23a3897c7f45afd8e6f77
June <junemvp@foxmail.com>
2021-12-30 17:21:12 +0800

    try to optimization viewBinding use

--- base/component/src/main/java/com/june/base/basic/part/BaseActivity.kt
+++ base/component/src/main/java/com/june/base/basic/part/BaseActivity.kt
@@ -1,8 +1,10 @@
 package com.june.base.basic.part
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
+import java.lang.reflect.ParameterizedType
 
 abstract class BaseActivity<V : ViewBinding> : AppCompatActivity() {
 
@@ -17,13 +19,15 @@ abstract class BaseActivity<V : ViewBinding> : AppCompatActivity() {
     }
 
     // 通过反射创建ViewBinding失败
-    //    private fun viewBinding2(): V {
-    //        val parameterizedType = this.javaClass.genericSuperclass as ParameterizedType
-    //        val clazz: Class<V> = parameterizedType.actualTypeArguments[0] as Class<V>
-    //        val argsClazz: Array<Class<out LayoutInflater>> = arrayOf(layoutInflater.javaClass)
-    //        val constructor: Constructor<V> = clazz.getConstructor(*argsClazz)
-    //        return constructor.newInstance(*argsClazz)
-    //    }
+//    private fun viewBinding2(): V {
+//        val parameterizedType = this.javaClass.genericSuperclass as ParameterizedType
+//        val clazz: Class<V> = parameterizedType.actualTypeArguments[0] as Class<V>
+//        val argsClazz: Array<Class<out LayoutInflater>> = arrayOf(layoutInflater.javaClass)
+//        clazz.getDeclaredMethod("",*argsClazz)
+//
+//        val constructor: Constructor<V> = clazz.getConstructor(*argsClazz)
+//        return constructor.newInstance(*argsClazz)
+//    }
 
     /**
      * 设置viewBinding
