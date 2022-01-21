4f0419b4937a7df52b3e540b5aed719b8e5ef378
June <junemvp@foxmail.com>
2022-01-21 10:52:08 +0800

    viewBinding获取替换

--- base/component/src/main/java/com/june/base/basic/part/BaseActivity.kt
+++ base/component/src/main/java/com/june/base/basic/part/BaseActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
+import timber.log.Timber
 import java.lang.reflect.ParameterizedType
 
 abstract class BaseActivity<V : ViewBinding> : AppCompatActivity() {
@@ -12,22 +13,29 @@ abstract class BaseActivity<V : ViewBinding> : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = viewBinding()
+        mBinding = viewBinding2()
         setContentView(mBinding.root)
         initView()
         loadData()
     }
 
     // 通过反射创建ViewBinding失败
-//    private fun viewBinding2(): V {
-//        val parameterizedType = this.javaClass.genericSuperclass as ParameterizedType
-//        val clazz: Class<V> = parameterizedType.actualTypeArguments[0] as Class<V>
-//        val argsClazz: Array<Class<out LayoutInflater>> = arrayOf(layoutInflater.javaClass)
-//        clazz.getDeclaredMethod("",*argsClazz)
-//
-//        val constructor: Constructor<V> = clazz.getConstructor(*argsClazz)
-//        return constructor.newInstance(*argsClazz)
-//    }
+    private fun viewBinding2(): V {
+        //获取 Java类的 ParameterizedType
+        val parameterizedType = this.javaClass.genericSuperclass as ParameterizedType
+
+        //通过 ParameterizedType 工具获得泛型具体类型
+        val clazz: Class<V> = parameterizedType.actualTypeArguments[0] as Class<V>
+
+        Timber.e("clazz 的类型：${clazz.simpleName}")
+
+        // 获取ViewBinding的inflate方法
+        val inflateMethod = clazz.getMethod("inflate", LayoutInflater::class.java)
+
+        Timber.e("inflateMethod 方法：${inflateMethod.name}")
+
+        return inflateMethod.invoke(null, layoutInflater) as V
+    }
 
     /**
      * 设置viewBinding
