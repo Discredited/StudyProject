c37d9ea5991ad4aa30274915346e125009d07b6e
June <junemvp@foxmail.com>
2022-01-21 10:55:22 +0800

    通过反射自动获取ViewBinding并赋值

--- base/component/src/main/java/com/june/base/basic/part/BaseActivity.kt
+++ base/component/src/main/java/com/june/base/basic/part/BaseActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
-import timber.log.Timber
 import java.lang.reflect.ParameterizedType
 
 abstract class BaseActivity<V : ViewBinding> : AppCompatActivity() {
@@ -21,19 +20,12 @@ abstract class BaseActivity<V : ViewBinding> : AppCompatActivity() {
 
     // 通过反射创建ViewBinding失败
     private fun viewBinding2(): V {
-        //获取 Java类的 ParameterizedType
+        // 获取 Java类的 ParameterizedType
         val parameterizedType = this.javaClass.genericSuperclass as ParameterizedType
-
-        //通过 ParameterizedType 工具获得泛型具体类型
+        // 通过 ParameterizedType 工具获得泛型具体类型
         val clazz: Class<V> = parameterizedType.actualTypeArguments[0] as Class<V>
-
-        Timber.e("clazz 的类型：${clazz.simpleName}")
-
         // 获取ViewBinding的inflate方法
         val inflateMethod = clazz.getMethod("inflate", LayoutInflater::class.java)
-
-        Timber.e("inflateMethod 方法：${inflateMethod.name}")
-
         return inflateMethod.invoke(null, layoutInflater) as V
     }
 
