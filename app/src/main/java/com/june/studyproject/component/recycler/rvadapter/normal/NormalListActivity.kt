67b386c16e019ea2a73e04ed49c26de7d977e9db
June <junemvp@foxmail.com>
2022-01-22 22:36:31 +0800

    NormalAdapter实现梳理

--- app/src/main/java/com/june/studyproject/component/recycler/rvadapter/normal/NormalListActivity.kt
+++ app/src/main/java/com/june/studyproject/component/recycler/rvadapter/normal/NormalListActivity.kt
@@ -25,17 +25,17 @@ class NormalListActivity : BaseActivity<ActivityNormalListBinding>() {
 
     override fun loadData() {
         mAdapter.setNewList(
-                mutableListOf(
-                        NormalSimpleText("迦叶和尚", "十六年，你的阳寿只有十六年"),
-                        NormalMultiText("林飞", "这是白马寺的大师伽叶第一次看见林飞时，对他说的话", "这一年，林飞十六岁"),
-                        NormalSimpleImage("甘柠真", ConstHelper.getDiffImage(1)[0]),
-                        NormalSimpleText("三美女相伴", "却令海姬失去了姐姐，给不了甘柠真安定与幸福，也可怜鸠丹媚随之流浪"),
-                        NormalMultiImage("丽人行", ConstHelper.getDiffImage(2)[0], ConstHelper.getDiffImage(2)[1]),
-                        NormalMultiText("林飞", "纵使不甘，却也无奈", "在林飞准备了却心头之愿、迎接命运时，神秘人出现，将他带入了北境，继而开始了林飞抗逆天命历程"),
-                        NormalSimpleText("天定的魔主", "沙罗铁树为之盛开"),
-                        NormalMultiText("楚度和阿萝", "误入龙鲸之腹，巧遇师傅阿萝", "随楚度清虚天一行，名为囚禁，实为教导，无奈却令沙罗铁树花开"),
-                        NormalSimpleImage("鸠丹媚", ConstHelper.getDiffImage(1)[0])
-                )
+            mutableListOf(
+                NormalSimpleText("迦叶和尚", "十六年，你的阳寿只有十六年"),
+                NormalMultiText("林飞", "这是白马寺的大师伽叶第一次看见林飞时，对他说的话", "这一年，林飞十六岁"),
+                NormalSimpleImage("甘柠真", ConstHelper.getDiffImage(1)[0]),
+                NormalSimpleText("三美女相伴", "却令海姬失去了姐姐，给不了甘柠真安定与幸福，也可怜鸠丹媚随之流浪"),
+                NormalMultiImage("丽人行", ConstHelper.getDiffImage(2)[0], ConstHelper.getDiffImage(2)[1]),
+                NormalMultiText("林飞", "纵使不甘，却也无奈", "在林飞准备了却心头之愿、迎接命运时，神秘人出现，将他带入了北境，继而开始了林飞抗逆天命历程"),
+                NormalSimpleText("天定的魔主", "沙罗铁树为之盛开"),
+                NormalMultiText("楚度和阿萝", "误入龙鲸之腹，巧遇师傅阿萝", "随楚度清虚天一行，名为囚禁，实为教导，无奈却令沙罗铁树花开"),
+                NormalSimpleImage("鸠丹媚", ConstHelper.getDiffImage(1)[0])
+            )
         )
         mAdapter.notifyDataSetChanged()
     }
