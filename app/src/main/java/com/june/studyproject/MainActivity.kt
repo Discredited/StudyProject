10c565582ce512d8e964f53b0f8073e49a400235
June <junemvp@foxmail.com>
2025-04-22 14:53:37 +0800

    [fix]:清理和简化项目代码

--- app/src/main/java/com/june/studyproject/MainActivity.kt
+++ app/src/main/java/com/june/studyproject/MainActivity.kt
@@ -1,9 +1,5 @@
 package com.june.studyproject
 
-import android.os.Build
-import android.view.View
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
 import com.june.studyproject.base.app.StudyBaseActivity
@@ -16,13 +12,9 @@ class MainActivity : StudyBaseActivity<ActivityMainBinding>() {
 
     override fun initView() {
         //mBinding.bottomNavigation.itemIconTintList = null
-
-        setStatusBarColor()
     }
 
     override fun loadData() {
-        //todo 了解和改造 BottomNavigation与NvaController的关联
-
         val navGraphIds = listOf(
             R.navigation.nav_component,
             R.navigation.nav_library,
@@ -39,16 +31,5 @@ class MainActivity : StudyBaseActivity<ActivityMainBinding>() {
 
     override fun onSupportNavigateUp(): Boolean {
         return mCurrentNavController?.value?.navigateUp() ?: false
-    }
-
-    private fun setStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            window?.isNavigationBarContrastEnforced = false
-            ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, windowInsets ->
-                val inserts = windowInsets.getInsets(WindowInsetsCompat.Type.statusBars())
-                v.setPadding(inserts.left, v.paddingTop, inserts.right, inserts.bottom)
-                WindowInsetsCompat.CONSUMED
-            }
-        }
     }
 }
